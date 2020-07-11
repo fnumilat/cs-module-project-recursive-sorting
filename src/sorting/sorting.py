d38f7e6eb06f8e114df2db87c9a971fefc93e9bb
unknown <fnumilat@gmail.com>
2020-07-11 13:55:09 -0500

    Met MVP

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -4,25 +4,59 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
-
-
+    left = right = index = 0
+    # For each item in subarray
+    while left < len(arrA) and right < len(arrB):
+        # Compare each value of each subarray, starting with the first
+        # If the value in the left array is lower, put that value in the merged array
+        if arrA[left] <= arrB[right]:
+            merged_arr[index] = arrA[left]
+            left += 1
+        # If the value in the right array is lower, put that value in the merged array
+        else:
+            merged_arr[index] = arrB[right]
+            right += 1
+        index += 1
+    # Add the leftovers in left and right to the merged array
+    while left < len(arrA):
+        merged_arr[index] = arrA[left]
+        left += 1
+        index += 1
+    while right < len(arrB):
+        merged_arr[index] = arrB[right]
+        right += 1
+        index += 1    
     return merged_arr
+
+
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
+    # Base Case: Array with one element
+    if len(arr) <= 1:
+        return arr
+    
+    # Recursive Case: 
+    # Divide in half until subarrays have only one element
+    # Find the middle point
+    # Divide the array in half
+    arrA = arr[:len(arr)//2]
+    arrB = arr[len(arr)//2:]
+    # Call merge on each half, recursively
+    # The recursion will break the arrays up until they are only one value
+    return merge(merge_sort(arrA), merge_sort(arrB))
 
-
-    return arr
+    
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
+# def merge_in_place(arr, start, mid, end):
+#     # Your code here
 
 
-def merge_sort_in_place(arr, l, r):
-    # Your code here
+# def merge_sort_in_place(arr, l, r):
+#     # Your code here
 
